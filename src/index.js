2c4f92b419d4722be0b962daab73bc42dfa9941a
jainChetan81 <jain.cj.chetan@gmail.com>
2021-08-03 10:15:15 +0530

    in working conditions without themes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-console.log("Its working");
+import "./styles/index.scss";
 
 const theme = localStorage.getItem("theme");
 
-if (theme == null) {
-	setTheme("light");
-} else {
-	setTheme(theme);
-}
+// if (theme == null) {
+// 	setTheme("light");
+// } else {
+// 	setTheme(theme);
+// }
 
 const themeDots = document.getElementsByClassName("theme-dot");
 
@@ -14,22 +14,22 @@ for (let i = 0; themeDots.length > i; i++) {
 	themeDots[i].addEventListener("click", function () {
 		const mode = this.dataset.mode;
 		console.log("Option clicked:", mode);
-		setTheme(mode);
+		// setTheme(mode);
 	});
 }
 
-function setTheme(mode) {
-	if (mode == "light") {
-		document.getElementById("theme-style").href = "default.css";
-	}
-	if (mode == "blue") {
-		document.getElementById("theme-style").href = "blue.css";
-	}
-	if (mode == "green") {
-		document.getElementById("theme-style").href = "green.css";
-	}
-	if (mode == "purple") {
-		document.getElementById("theme-style").href = "purple.css";
-	}
-	localStorage.setItem("theme", mode);
-}
+// function setTheme(mode) {
+// 	if (mode == "light") {
+// 		document.getElementById("theme-style").href = "default.css";
+// 	}
+// 	if (mode == "blue") {
+// 		document.getElementById("theme-style").href = "blue.css";
+// 	}
+// 	if (mode == "green") {
+// 		document.getElementById("theme-style").href = "green.css";
+// 	}
+// 	if (mode == "purple") {
+// 		document.getElementById("theme-style").href = "purple.css";
+// 	}
+// 	localStorage.setItem("theme", mode);
+// }
--- webpack.dev.js
+++ webpack.dev.js
@@ -11,17 +11,52 @@ module.exports = merge(common, {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: "webpack Boilerplate",
-			favicon: paths.src + "/assets/webpack.svg",
+			title: "Chetan's Portfolio",
+			favicon: paths.src + "/images/ChetanId.jpg",
 			template: paths.src + "/template.html", // template file
 			filename: "index.html", // output file
 			inject: true,
 		}),
 		new HtmlWebpackPlugin({
-			title: "Burger Builder Description",
-			favicon: paths.src + "/assets/webpack.svg",
-			template: paths.src + "/burger.html", // template file
+			title: "Burger Builder",
+			favicon: paths.src + "/images/ChetanId.jpg",
+			template: paths.src + "/project/burger.html", // template file
 			filename: "burger.html", // output file
+			inject: true,
+		}),
+		new HtmlWebpackPlugin({
+			title: "Covid19 Tracker",
+			favicon: paths.src + "/images/ChetanId.jpg",
+			template: paths.src + "/project/covid19.html", // template file
+			filename: "covid19.html", // output file
+			inject: true,
+		}),
+		new HtmlWebpackPlugin({
+			title: "Slack Clone",
+			favicon: paths.src + "/images/ChetanId.jpg",
+			template: paths.src + "/project/devchat.html", // template file
+			filename: "devchat.html", // output file
+			inject: true,
+		}),
+		new HtmlWebpackPlugin({
+			title: "Ecommerce Website",
+			favicon: paths.src + "/images/ChetanId.jpg",
+			template: paths.src + "/project/ecommerce.html", // template file
+			filename: "ecommerce.html", // output file
+			inject: true,
+		}),
+		new HtmlWebpackPlugin({
+			title: "Survey Website",
+			favicon: paths.src + "/images/ChetanId.jpg",
+			template: paths.src + "/project/emaily.html", // template file
+			filename: "emaily.html", // output file
+			inject: true,
+		}),
+		new HtmlWebpackPlugin({
+			title: "News Website",
+			favicon: paths.src + "/images/ChetanId.jpg",
+			template: paths.src + "/project/news.html", // template file
+			filename: "news.html", // output file
 			inject: true,
 		}),
 	],

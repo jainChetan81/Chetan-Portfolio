534dba06ebf61c055890984ef0f977e2473ba327
jainChetan81 <jain.cj.chetan@gmail.com>
2021-08-06 02:03:20 +0530

    removed some console.logs

--- src/index.js
+++ src/index.js
@@ -26,23 +26,25 @@ function setTheme(mode) {
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", () => {
 		const wb = new Workbox("/service-worker.js");
-		const updateButton = document.querySelector("#app-update");
-		// Fires when the registered service worker has installed but is waiting to activate.
-		wb.addEventListener("waiting", () => {
-			updateButton.classList.add("show");
-			updateButton.addEventListener("click", () => {
-				// Set up a listener that will reload the page as soon as the previously waiting service worker has taken control.
-				wb.addEventListener("controlling", () => {
-					window.location.reload();
+		if (wb && (wb != null || undefined)) {
+			const updateButton = document.querySelector("#app-update");
+			// Fires when the registered service worker has installed but is waiting to activate.
+			wb.addEventListener("waiting", () => {
+				updateButton.classList.add("show");
+				updateButton.addEventListener("click", () => {
+					// Set up a listener that will reload the page as soon as the previously waiting service worker has taken control.
+					wb.addEventListener("controlling", () => {
+						window.location.reload();
+					});
+
+					// Send a message telling the service worker to skip waiting.
+					// This will trigger the `controlling` event handler above.
+					wb.messageSW({ type: "SKIP_WAITING" });
 				});
-
-				// Send a message telling the service worker to skip waiting.
-				// This will trigger the `controlling` event handler above.
-				wb.messageSW({ type: "SKIP_WAITING" });
 			});
-		});
 
-		wb.register();
+			wb.register();
+		}
 	});
 }
 

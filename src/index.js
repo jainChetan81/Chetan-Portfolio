5e07f14891a6b7de8a1b97201f01fd37d9344c57
jainChetan81 <jain.cj.chetan@gmail.com>
2021-08-06 03:51:46 +0530

    finally service worker is registering and we can move on

--- src/index.js
+++ src/index.js
@@ -46,10 +46,9 @@ document.getElementById("contact-form").addEventListener("submit", function (eve
 });
 if ("serviceWorker" in navigator) {
 	window.addEventListener("load", function () {
-		navigator.serviceWorker
-			.register("./service-worker.js")
-			.then((res) => console.log("service worker registered"))
-			.catch((err) => console.log("service worker not registered", err));
+		navigator.serviceWorker.register("./service-worker.js");
+		// .then((res) => console.log("service worker registered"))
+		// .catch((err) => console.log("service worker not registered", err));
 	});
 }
 
